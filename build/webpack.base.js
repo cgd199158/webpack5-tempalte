8c9343cf60c9b3680a5515b3b0c98d1a38c387de
cgd <349008059@qq.com>
2022-06-28 18:21:11 +0800

    feat: 添加fork-ts-checker-webpack-plugin提示编译速度并添加commitlint插件
    测试提交
    
    测试提交
    
    测试提交
    
    测试提交
    
    commitlint
    
    foo: this will fail
    
    feat: commitlint

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -2,6 +2,7 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const { VueLoaderPlugin } = require('vue-loader');
 const { resolve } = require('path');
 const config = require('./config.js');
@@ -175,5 +176,7 @@ module.exports = {
       template: resolve(__dirname, '/public/index.html'),
       hash: true, // 破坏缓存
     }),
+    // fork-ts-checker-webpack-plugin，顾名思义就是创建一个新进程，专门来运行Typescript类型检查。这么做的原因是为了利用多核资源来提升编译的速度
+    new ForkTsCheckerWebpackPlugin(),
   ],
 };
