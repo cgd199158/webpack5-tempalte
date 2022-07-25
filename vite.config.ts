09aaf9f429473efe0b9c3131c78e5d6f2042c6a8
cgd <349008059@qq.com>
2022-07-25 14:41:32 +0800

    feat: 添加本地服务代理

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,11 @@ import { resolve } from 'path';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 import AutoImport from 'unplugin-auto-import/vite';
+import viteImagemin from 'vite-plugin-imagemin';
+// 在服务端获取配置参数
 
 export default defineConfig({
+  base: '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
@@ -16,6 +19,17 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         additionalData: '@import "./src/styles/vars.scss";',
+      },
+    },
+  },
+  server: {
+    port: 9003,
+    host: true,
+    proxy: {
+      '^/api/.*': {
+        target: 'http://192.168.2.16', // 内网地址（线上开发打开）
+        changeOrigin: true, // 支持跨域
+        rewrite: (path) => path.replace(/^\/api/, '/'),
       },
     },
   },
@@ -30,5 +44,37 @@ export default defineConfig({
       imports: ['vue', 'vue-router'],
       dts: resolve(__dirname, './src/types/auto-import.d.ts'),
     }),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox',
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false,
+          },
+        ],
+      },
+    }),
   ],
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
 });
